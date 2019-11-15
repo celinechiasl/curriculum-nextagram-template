631bbb4c6c2f2a09e5a8f3525ebee6b66cb6df3f
celinechiasl <celinechia.s.l@gmail.com>
2019-11-15 15:33:25 +0800

    Add sign up feature

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, flash, url_for, request
+from werkzeug.security import generate_password_hash
+from models.user import * #for postbird to read the user list
 
 
 users_blueprint = Blueprint('users',
@@ -11,9 +13,20 @@ def new():
     return render_template('users/new.html')
 
 
-@users_blueprint.route('/', methods=['POST'])
+@users_blueprint.route('/create', methods=['POST'])
 def create():
-    pass
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    hashed_password = generate_password_hash(password)
+    # pop out noti when successful signed up
+    successsignedup = User(username=username, email=email, password=hashed_password)
+    if successsignedup.save() and password != "":
+        flash('Successfully signed up!', "success")
+        return redirect(url_for('users.new')) #this users.new is referred to def new() as above
+    else:
+        flash('Registration failed. Username has been taken.', "danger")
+        return render_template ('/users/new.html')
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
